ec8793e41332378dd9a0942802f5fa8966898147
Karl Price <karuprice@gmail.com>
2017-08-01 22:38:20 +0100

    section 12 finished

--- app/app.js
+++ app/app.js
@@ -6,6 +6,34 @@ myNinjaApp.controller('ninjaController', ['$scope', function($scope) {
 
   $scope.message = 'Ninja time'
 
-  $scope.ninjas = ['Karl', 'Kayo', 'Liam', 'Evan']
+  $scope.ninjas = [
+    {
+      name: 'Karl',
+      belt: 'Black',
+      rate: 50000,
+      availiable: true
+    },
+    {
+      name: 'Kayo',
+      belt: 'Red',
+      rate: 4500,
+      availiable: true
+    },
+    {
+      name: 'Liam',
+      belt: 'Blue',
+      rate: 3000,
+      availiable: false
+    },
+    {
+      name: 'Evan',
+      belt: 'Yellow',
+      rate: 2000,
+      availiable: true
+    },
+  ]
+
+
+
 
 }]);
