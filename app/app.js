3335f5caa7fdcc4eaa1ef421cc3b2e5550aa398e
Karl Price <karuprice@gmail.com>
2017-08-02 07:28:44 +0100

    section 15 images added

--- app/app.js
+++ app/app.js
@@ -9,36 +9,51 @@ myNinjaApp.controller('ninjaController', ['$scope', function($scope) {
   $scope.removeNinja = function(ninja){
     var removedNinja = $scope.ninjas.indexOf(ninja);
     $scope.ninjas.splice(removedNinja, 1);
-  }
+  };
+
+  $scope.addNinja = function(){
+    $scope.ninjas.push({
+      name: $scope.newNinja.name,
+      belt: $scope.newNinja.belt,
+      rate: parseInt($scope.newNinja.rate),
+      availiable: true
+    });
+
+    $scope.newNinja.name = '',
+    $scope.newNinja.belt = '',
+    $scope.newNinja.rate = ''
+
+  };
 
   $scope.ninjas = [
     {
       name: 'Karl',
       belt: 'Black',
       rate: 50000,
-      availiable: true
+      availiable: true,
+      thumb: "content/img/baby.png"
     },
     {
       name: 'Kayo',
       belt: 'Red',
       rate: 4500,
-      availiable: true
+      availiable: true,
+      thumb: "content/img/baby.png"
     },
     {
       name: 'Liam',
       belt: 'Blue',
       rate: 3000,
-      availiable: false
+      availiable: true,
+      thumb: "content/img/baby.png"
     },
     {
       name: 'Evan',
       belt: 'Yellow',
       rate: 2000,
-      availiable: true
+      availiable: true,
+      thumb: "content/img/baby.png"
     },
   ]
-
-
-
-
+  
 }]);
